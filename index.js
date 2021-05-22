3be81225c62140bc6c0e0d2445c0b5d2b3dd92c1
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-22 17:07:56 +0800

    added a command to run snort.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { Telegraf } = require('telegraf')
+const fs = require('fs')
 
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
-    ctx.reply('Hello there, I\'m bishamon. Type /help to see more commands.')
+    ctx.reply('Hello there, I\'m Bishamon. Type /help to see more commands.')
+})
+
+bot.command('startsnort', ctx => {
+    require('./commands/startSnort')
+    ctx.reply('snort run successfully.')
 })
 
 bot.launch()
