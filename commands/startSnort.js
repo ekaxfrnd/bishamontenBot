d4d8cf106fa63323c8655d582d3e2fd7d55029a6
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-24 00:32:00 +0800

    change the script to be asynchronous.

--- commands/startSnort.js
+++ commands/startSnort.js
@@ -1,28 +1,45 @@
-const { exec } = require('child_process')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 module.exports = {
-    startSnort: exec('sudo snort -u snort -g snort -i eth0 -c /etc/snort/snort.conf -q -A full -D', (err, stdout, stderr) => {
-        if(err) {
-            console.log(`err: ${err.message}`)
-            return
-        }
-        if(stdout) {
-            console.log(`snort run successfully.`)
-        }
-        if(stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-    }),
-    startWatch: exec('watch -n 5 tail -n 20 /var/log/snort/alert > ../../snort.log &', (err, stdout, stderr) => {
-        if(err) {
-            console.log(`err: ${err.message}`)
-            return
-        }
-        if(stdout) {
-            console.log(`watch run successfully.`)
-        }
-        if(stderr) {
-            console.log(`stderr: ${stderr}`)
-        }
-    })
+    startSnort: async () => {
+        const { stdout, stderr } = await exec('sudo snort -u snort -g snort -i eth0 -c /etc/snort/snort.conf -q -A full -D')
+        console.log(`stdout: `, stdout)
+        console.log(`stderr: `, stderr)
+    },
+    startWatch: async () => {
+        const { stdout, stderr } = await exec('watch -n 5 tail -n 20 /var/log/snort/alert > ../../snort.log')
+        console.log(`stdout: `, stdout)
+        console.log(`stderr: `, stderr)
+    }
 }
+
+// const { exec } = require('child_process')
+// const 
+// 
+// module.exports = {
+    // // startSnort: exec('sudo snort -u snort -g snort -i eth0 -c /etc/snort/snort.conf -q -A full -D', (err, stdout, stderr) => {
+        // if(err) {
+            // console.log(`err: ${err.message}`)
+            // return
+        // }
+        // if(stdout) {
+            // console.log(`snort run successfully.`)
+        // }
+        // if(stderr) {
+            // console.log(`stderr: ${stderr}`);
+        // }
+    // }),
+    // // startWatch: exec('watch -n 5 tail -n 20 /var/log/snort/alert > ../../snort.log &', (err, stdout, stderr) => {
+        // if(err) {
+            // console.log(`err: ${err.message}`)
+            // return
+        // }
+        // if(stdout) {
+            // console.log(`watch run successfully.`)
+        // }
+        // if(stderr) {
+            // console.log(`stderr: ${stderr}`)
+        // }
+    // })
+// }
