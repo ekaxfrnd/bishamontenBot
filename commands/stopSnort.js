fd5b0071d81019c502a28e6d3fc24a0eb193c6cc
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-24 02:33:40 +0800

    script to stop snort.

---
+++ commands/stopSnort.js
@@ -0,0 +1,20 @@
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+module.exports = {
+    stopSnort: async () => {
+        const { stdout, stderr } = await exec('sudo killall snort')
+        console.log(`stdout: `, stdout)
+        console.log(`stderr: `, stderr)
+    },
+    stopWatch: async () => {
+        const { stdout, stderr } = await exec('killall watch')
+        console.log(`stdout: `, stdout)
+        console.log(`stderr: `, stderr)
+    },
+    stopNode: async () => {
+        const { stdout, stderr } = await exec('killall node')
+        console.log(`stdout: `, stdout)
+        console.log(`stderr: `, stderr)
+    }
+}
