b0f268912d8b018b146d7bb2c4a680715b28f59d
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-23 10:27:08 +0800

    call direct command from exec.

--- commands/startSnort.js
+++ commands/startSnort.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process')
 
-const startSnort = exec('bash ./util/startsnort.sh', (err, stdout, stderr) => {
+const startSnort = exec('sudo snort -u snort -g snort -i eth0 -c /etc/snort/snort.conf -q -A full -D && watch -n 5 tail -n 20 /var/log/snort/alert > ../../snort.log &', (err, stdout, stderr) => {
     if(err) {
         console.log(`err: ${err.message}`)
     }
