85dca14ac8b6cc415bfc894966b89954e0e4d886
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-23 10:51:50 +0800

    split scripts to initiate snort and watch.

--- commands/startSnort.js
+++ commands/startSnort.js
@@ -1,12 +1,28 @@
 const { exec } = require('child_process')
 
-const startSnort = exec('sudo snort -u snort -g snort -i eth0 -c /etc/snort/snort.conf -q -A full -D && watch -n 5 tail -n 20 /var/log/snort/alert > ../../snort.log &', (err, stdout, stderr) => {
-    if(err) {
-        console.log(`err: ${err.message}`)
-    }
-    if(stdout) {
-        console.log(`snort run successfully.`)
-    }
-})
-
-module.exports = startSnort
+module.exports = {
+    startSnort: exec('sudo snort -u snort -g snort -i eth0 -c /etc/snort/snort.conf -q -A full -D', (err, stdout, stderr) => {
+        if(err) {
+            console.log(`err: ${err.message}`)
+            return
+        }
+        if(stdout) {
+            console.log(`snort run successfully.`)
+        }
+        if(stderr) {
+            console.log(`stderr: ${stderr}`);
+        }
+    }),
+    startWatch: exec('watch -n 5 tail -n 20 /var/log/snort/alert > ../../snort.log &', (err, stdout, stderr) => {
+        if(err) {
+            console.log(`err: ${err.message}`)
+            return
+        }
+        if(stdout) {
+            console.log(`watch run successfully.`)
+        }
+        if(stderr) {
+            console.log(`stderr: ${stderr}`)
+        }
+    })
+}
