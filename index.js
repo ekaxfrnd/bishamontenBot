a40904adc5792539e37a33c6cfc205372b01d79e
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-24 00:33:12 +0800

    directly call up file to start snort.

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const command = require('./commands/index')
+// const command = require('./commands/index')
 
 bot.start(ctx => {
     ctx.reply('Hello there, I\'m Bishamon. Type /help to see more commands.')
 })
 
 bot.command('startsnort', ctx => {
-    command.startSnort
-    if(command.startSnort) {
-        ctx.reply('snort run successfully.')
-    }
+    require('./commands/startSnort')
+    // command.startSnort
+    ctx.reply('snort run successfully.')
 })
 
 bot.launch()
