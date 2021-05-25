b7b889509814c8710e2e2649ac8313f2647637b7
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-25 13:19:19 +0800

    message for help.

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+module.exports {
+    help: `I can help you monitor snort logs from telegram.
+
+You can control me by sending these commands:
+
+bold General
+/start - start the bot
+/help - show this help
+
+bold Snort
+/snortstart - run snort
+/snortstop - stop snort
+/snortrestart - restart snort
+/snortstatus - check snort status
+
+bold Log
+/logstart - start displaying the snort log`
+}
