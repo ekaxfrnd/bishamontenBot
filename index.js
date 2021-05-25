9323dbe2a92c6debdc9e45e87523c6233a805705
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-25 13:14:49 +0800

    added help command.

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+const helpMessage = require('./commands/help')
 const startSnort = require('./commands/startSnort')
 const stopSnort = require('./commands/stopSnort')
 
 bot.start(ctx => {
     ctx.reply('Hello there, I\'m Bishamon. Type /help to see more commands.')
+})
+
+bot.help(ctx => {
+    ctx.reply(helpMessage)
 })
 
 bot.command('startsnort', ctx => {
