219ceeb461eb2ad0402982ed67562d198c1cda1b
ekaxfrnd <ekaxfrnd@gmail.com>
2021-05-27 13:48:55 +0800

    added command restart snort and change code to async await.

--- index.js
+++ index.js
@@ -17,16 +17,36 @@ bot.help(ctx => {
     ctx.reply(messages.help)
 })
 
-bot.command('snortstart', ctx => {
-    snortStart.snortStart()
-    snortStart.watchStart()
-    ctx.reply('snort run successfully.')
+bot.command('snortstart', async ctx => {
+    try {
+        await snortStart.snortStart()
+        await snortStart.watchStart()
+        ctx.reply('snort run successfully.')
+    } catch (err) {
+        ctx.reply('snort failed to start.')
+    }
 })
 
-bot.command('snortstop', ctx => {
-    snortStop.snortStop()
-    snortStop.watchStop()
-    ctx.reply('snort quit successfully.')
+bot.command('snortstop', async ctx => {
+    try {
+        await snortStop.snortStop()
+        await snortStop.watchStop()
+        ctx.reply('snort quit successfully.')
+    } catch (err) {
+        ctx.reply('snort failed to stop.')
+    }
+})
+
+bot.command('snortrestart', async ctx => {
+    try {
+        await snortStart.snortStart()
+        await snortStart.watchStart()
+        await snortStop.snortStop()
+        await snortStop.watchStop()
+        ctx.reply('snort has restarted successfully.')
+    } catch (err) {
+        ctx.reply('snort failed to restart.')
+    }
 })
 
 bot.launch()
