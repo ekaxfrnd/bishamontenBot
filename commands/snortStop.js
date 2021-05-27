d39cfba170b07344ae03f72a41af6ac35e616199
corn404 <codecorn404@gmail.com>
2021-05-27 16:26:40 +0800

    new update

---
+++ commands/snortStop.js
@@ -0,0 +1,23 @@
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+module.exports = {
+    snortStop: async () => {
+        const { stdout, stderr } = await exec('sudo killall snort')
+        if(stdout) {
+            console.log(`stdout: `, stdout)
+        }
+        if(stderr) {
+            console.log(`stderr: `, stderr)
+        }
+    },
+    watchStop: async () => {
+        const { stdout, stderr } = await exec('killall watch')
+        if(stdout) {
+            console.log(`stdout: `, stdout)
+        }
+        if(stderr) {
+            console.log(`stderr: `, stderr)
+        }
+    }
+}
