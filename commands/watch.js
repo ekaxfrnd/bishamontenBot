4a645488a9bae0fff26aab8d84ed2461e22e4939
ekaxfrnd <ekaxfrnd@gmail.com>
2021-06-02 13:59:54 +0800

    merge some terminal commands into a file.

--- commands/watch.js
+++ commands/watch.js
@@ -4,8 +4,20 @@ const exec = util.promisify(require('child_process').exec)
 module.exports = {
     watchStart: async () => {
         const { stdout, stderr } = await exec('watch -n 1 tail -n 7 /var/log/snort/alert > snort.log &')
+        if(stdout) {
+            console.log(`stdout: ${stdout}`)
+        }
+        if(stderr) {
+            console.log(`stderr: ${stderr}`)
+        }
     },
     watchStop: async () => {
         const { stdout, stderr } = await exec('killall watch')
+        if(stdout) {
+            console.log(`stdout: ${stdout}`)
+        }
+        if(stderr) {
+            console.log(`stderr: ${stderr}`)
+        }
     }
 }
